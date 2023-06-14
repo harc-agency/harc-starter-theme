cf7b34c90fd10baca73958efef4f47f28f52a305
Dustin Woodard <dustin.woodard@gmail.com>
2023-06-14 14:13:25 -0600

    updates from storkmentors

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,24 @@ mix.js('src/js/app.js', 'dist')
     .webpackConfig({
         resolve: {
             alias: {
+                '~': path.resolve(__dirname),
                 '@': path.resolve('src/js'),
+
             },
         },
     })
     .version()
+
+//browserSync
+mix.browserSync({
+    proxy: 'storkmentors.local',
+    files: [
+
+        'dist/**/*', //src files
+        '**/*.php', //wordpress files
+        'src/css/*.css',
+    ],
+    open: true, // Don't automatically open the browser after running Browsersync
+    notify: false, // Don't show any notifications in the browser.
+    ghostMode: false, // Disable syncing multiple tabs
+})
