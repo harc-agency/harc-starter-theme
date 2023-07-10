d9db6a67667b710c8484ac27607a7b3615bda1a0
Dustin Woodard <dustin.woodard@gmail.com>
2023-07-10 11:39:01 -0600

    main nav

--- src/js/Shared/Header.vue
+++ src/js/Shared/Header.vue
@@ -1,19 +1,34 @@
 <template>
 <header style="display: flex; align-items: center; justify-content: space-between">
-    <Link href="/">
-        <h1>{{ props.site.name }}</h1>
-    </Link>
-    <nav>
-        <Link href="/this-page-does-not-exist">404 Page</Link>
-        <Link style="margin-left: 1rem" href="/sample-page">Sample Page</Link>
-    </nav>
+  <Link href="/">
+    <h1>{{ props.site.name }}</h1>
+  </Link>
+  <nav>
+    <ul>
+      <li tabindex="0" v-for=" (link, index) in page.props.menu" :key="index">
+        <Link>{{ link.title }}</Link>
+      </li>
+    </ul>
+
+    <Link href="/this-page-does-not-exist">404 Page</Link>
+    <Link style="margin-left: 1rem" href="/sample-page">Sample Page</Link>
+  </nav>
 </header>
 </template>
 
 <script setup>
+import Logo from "@/Shared/Logo.vue"
 import { Link, usePage } from "@inertiajs/inertia-vue3"
+import { onMounted, reactive } from "vue"
 
-const { props } = usePage()
+const page = reactive(usePage())
+
+//make reactive
+const isCurrentUrl = url => (new URL(url, window.location.origin)).pathname === window.location.pathname
+
+onMounted(() => {
+
+})
 </script>
 
 <style></style>
